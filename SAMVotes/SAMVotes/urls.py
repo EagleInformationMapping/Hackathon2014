c1014a31a3f81d4c96534792cce41e15cea76088
rcolburn <rcolburn@eaglemap.com>
2014-03-09 23:23:47 -0500

    commit

--- SAMVotes/SAMVotes/urls.py
+++ SAMVotes/SAMVotes/urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^mobile/', MobileApp.as_view()),
+    url(r'^', MobileApp.as_view()),
 )
