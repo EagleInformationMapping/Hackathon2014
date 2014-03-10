90abf873942a16d4447f80e900ca3acda0b97cbe
rcolburn <rcolburn@eaglemap.com>
2014-03-09 23:55:19 -0500

    twiliotest

--- SAMVotes/SAMVotes/urls.py
+++ SAMVotes/SAMVotes/urls.py
@@ -18,4 +18,7 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^', MobileApp.as_view()),
+    url(r'^hello_world/$', 'django_twilio.views.say', {
+        'text': 'Hello, world!'
+    }),
 )
