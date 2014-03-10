d376eac47d1a4ce981379d2712795ee6ed9f0a12
rcolburn <rcolburn@eaglemap.com>
2014-03-10 00:10:36 -0500

    j

--- SAMVotes/SAMVotes/settings.py
+++ SAMVotes/SAMVotes/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'vote_app',
+    'django_twilio',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- SAMVotes/vote_app/views.py
+++ SAMVotes/vote_app/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
-
+import django_twilio
 from django.views.generic import TemplateView
 
 
 class MobileApp(TemplateView):
     template_name = 'mobile_app.html'
+
+
