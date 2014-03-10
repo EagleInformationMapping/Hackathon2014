3d2ec2e6513b45ccfd54fd49dcb55130373ff34a
rcolburn <rcolburn@eaglemap.com>
2014-03-10 04:55:53 -0500

    asdf

--- SAMVotes/vote_app/views.py
+++ SAMVotes/vote_app/views.py
@@ -12,8 +12,8 @@ def reply_to_sms_messages(request):
     # t.run('what up!!!')
     # r = '<Response><Sms>Thanks for the SMS message!</Sms></Response>'
     l = list()
-    for key, value in request.GET.iteritems():
-        l.append((key),(value))
-    
+    for i in request.GET.keys():
+        l.append(i)
+
     r = HttpResponse('<Response><Sms>Your phone number is {}</Sms></Response>'.format(l), 'text/XML')
     return r
