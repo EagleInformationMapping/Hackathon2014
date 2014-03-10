b57204441c151b23994d8d04aa10fd67904e0087
rcolburn <rcolburn@eaglemap.com>
2014-03-10 01:42:58 -0500

    asddf

--- SAMVotes/SAMVotes/urls.py
+++ SAMVotes/SAMVotes/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-# from vote_app.views import reply_to_sms_messages
+from vote_app.views import reply_to_sms_messages
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -22,9 +22,9 @@ urlpatterns = patterns('',
         'text': 'Hello, world!'
     }),
 
-    # url(r'^sms/$', reply_to_sms_messages, {
-    #     'text': 'Hello, world!'
-    # }),
+    url(r'^sms/$', reply_to_sms_messages, {
+        'text': 'Hello, world!'
+    }),
 
     url(r'^', MobileApp.as_view()),
 )
--- SAMVotes/vote_app/views.py
+++ SAMVotes/vote_app/views.py
@@ -1,14 +1,14 @@
 # Create your views here.
-# from twilio.twiml import Response
-# from django_twilio.decorators import twilio_view
+from twilio.twiml import Response
+from django_twilio.decorators import twilio_view
 from django.views.generic import TemplateView
 
 
 class MobileApp(TemplateView):
     template_name = 'mobile_app.html'
 
-# @twilio_view
-# def reply_to_sms_messages(request):
-#     r = Response()
-#     r.sms('Thanks for the SMS message!')
-#     return r
+@twilio_view
+def reply_to_sms_messages(request):
+    r = Response()
+    r.sms('Thanks for the SMS message!')
+    return r
