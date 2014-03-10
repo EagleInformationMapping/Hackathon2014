a9ce46bd4146d7d1802b643548a6e4aa9e21b90d
rcolburn <rcolburn@eaglemap.com>
2014-03-10 04:24:43 -0500

    asdf

--- SAMVotes/vote_app/views.py
+++ SAMVotes/vote_app/views.py
@@ -1,20 +1,16 @@
 # Create your views here.
-# from twilio.twiml import Response
-# from django_twilio.decorators import twilio_view
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-# from vote_app.test import TextPhone
-
 
 class MobileApp(TemplateView):
     template_name = 'mobile_app.html'
 
-# @twilio_view
 def reply_to_sms_messages(request):
     # r = Response()
     # r.sms('Thanks for the SMS message!')
     # t = TextPhone()
     # t.run('what up!!!')
     # r = '<Response><Sms>Thanks for the SMS message!</Sms></Response>'
-    r = HttpResponse('<Response><Sms>!working</Sms></Response>', 'text/XML')
+    p = request._get('From')
+    r = HttpResponse('<Response><Sms>Your phone number is {}</Sms></Response>'.format(p), 'text/XML')
     return r
